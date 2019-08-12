b016ea1ebd4829be22bf0ee9385481d5dd814a54
Andrew Peterson and Dominique Youmans <dominique@>
2019-08-12 16:29:15 -0700

    adds tries to add sentence recording and capitalizing first and last

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var myWords = prompt("What would you like to say to me?");
+alert(myWords);
+var myWords = prompt("What would you like to say to me?");
+
+int stringLength = word.length();
+   String letter1 = word.substring (0,1);
+   String lastletter = word.substring ((stringLength-1),(stringLength));
+   String newWord =letter1.toUpperCase() + lastletter.toUpperCase();
+
+newWord = letter1.toUpperCase()
+                + word.substring(1, word.length() - 1)
+                + lastletter.toUpperCase();
